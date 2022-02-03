114d26b632abedf725562311373e8f30d15ca266
Emma Egstad <emma@egstad.com>
2022-02-02 18:36:10 -0600

    updating timeout

--- src/App.test.js
+++ src/App.test.js
@@ -3,14 +3,14 @@ import App from './App';
 
 test('renders page title', async () => {
   render(<App />);
-  await waitForElementToBeRemoved(await screen.findByText('Loading...'), { timeout: 2000 });
+  await waitForElementToBeRemoved(await screen.findByText('Loading...'), { timeout: 3000 });
   const pageTitle = screen.getByRole('heading', { name: /Creatures of Hyrule/i });
   expect(pageTitle).toBeInTheDocument();
 });
 
 test('renders creature cards', async () => {
   render(<App />);
-  await waitForElementToBeRemoved(await screen.findByText('Loading...'), { timeout: 2000 });
+  await waitForElementToBeRemoved(await screen.findByText('Loading...'), { timeout: 3000 });
   const headings = await screen.findAllByRole('heading');
   expect(headings).toHaveLength(48);
 });
