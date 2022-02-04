958902e5f454c5b8a23b227b4ed3702fa7576dbc
Emma Egstad <emma@egstad.com>
2022-02-03 18:57:42 -0600

    cleaning up code

--- src/services/creatures.js
+++ src/services/creatures.js
@@ -22,6 +22,5 @@ export async function fetchNonFoodCreatures() {
 export async function fetchCreatureById(id) {
   const resp = await fetch(`https://botw-compendium.herokuapp.com/api/v2/entry/${id}`);
   const data = await resp.json();
-  console.log(data);
   return data;
 }
